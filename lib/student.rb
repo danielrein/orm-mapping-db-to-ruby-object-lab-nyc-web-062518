0a9a464f86d446697644b09210dfd2783798e4a8
Daniel Ran <danielr1735@gmail.com>
2018-07-09 02:23:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
   end
 
   def self.students_below_12th_grade
-    run_sql('SELECT * FROM students WHERE grade < ?', 12).first
+    run_sql('SELECT * FROM students WHERE grade < ?', 12)
   end
 
   def save
