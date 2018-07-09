86fa1dc461ace35653bde45d5fef0e32ede2c42e
Daniel Ran <danielr1735@gmail.com>
2018-07-09 02:38:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,8 +47,8 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    array = run_sql('SELECT * FROM students WHERE grade = ? LIMIT ?', 10, 1)
-    self.new_from_db(array[0])
+    row = run_sql('SELECT * FROM students WHERE grade = ? LIMIT ?', 10, 1)[0]
+    self.new_from_db(row)
     # binding.pry
   end
 
