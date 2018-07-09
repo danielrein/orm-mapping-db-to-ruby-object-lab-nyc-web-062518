f388346a6a7ab12238075bc208f2265ff9a9adac
Daniel Ran <danielr1735@gmail.com>
2018-07-09 02:21:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    run_sql('SELECT * FROM students WHERE grade = ?', 9).length
+    run_sql('SELECT * FROM students WHERE grade = ?', 9)
   end
 
   def save
